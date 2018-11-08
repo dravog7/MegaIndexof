a9d38a72ed412643eeb79e228735a1a99bb68714
dravog7 <dravog77@gmail.com>
2018-11-08 23:32:44 +0530

    aa

--- mains/models.py
+++ mains/models.py
@@ -5,7 +5,7 @@ class settings(models.Model):
     maintenance=models.BooleanField(default=False)
 
 class website(models.Model):
-    url=models.URLField(max_length=1000)
+    url=models.URLField(max_length=1000,unique=True)
     noof=models.BigIntegerField(default=0)
 
     def __str__(self):
@@ -17,7 +17,7 @@ class videos(models.Model):
     season=models.IntegerField(default=1)
     episode=models.IntegerField(default=1)
     quality=models.TextField(default='0p')
-    url=models.URLField(max_length=1000)
+    url=models.URLField(max_length=1000,unique=True)
 
     def __str__(self):
         return "%s s%de%d %s"%(self.show,self.season,self.episode,self.quality)
