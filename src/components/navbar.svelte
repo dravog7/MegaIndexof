16fc498412e69134aeb2526ea2adbfbd143f56ba
John Abraham <31211464+dravog7@users.noreply.github.com>
2020-03-13 10:32:18 +0530

    Azure (#2)
    * interfacing svelte and django
    
    * added travis
    
    * changed travis yml
    
    * restructure files
    
    * changed travis
    
    * change travis

---
+++ src/components/navbar.svelte
@@ -0,0 +1,50 @@
+<script>
+import { link } from "svelte-routing";
+import {getContext} from 'svelte';
+import {ROUTER} from 'svelte-routing/src/contexts';
+import Ham from './hamburger.svelte'
+var { activeRoute } = getContext(ROUTER);
+let expanded=0;
+let currRoute;
+$: {
+    if($activeRoute){
+        currRoute = $activeRoute.uri;
+    console.log($activeRoute);
+    }
+}
+</script>
+
+<style lang="text/postcss">
+a:hover, a:visited, a:link, a:active{
+    color: black;
+    text-decoration: none;
+}
+
+.nav {
+    opacity: 0.75;
+}
+
+.nav:hover {
+    opacity: 1;
+}
+
+li {
+    @apply p-4;
+}
+</style>
+
+<div class="nav font-mono fixed bg-gray-200 rounded-r-lg lg:w-screen">
+    <div class="lg:hidden p-4">
+    <Ham on:switch="{e=>expanded=!e.detail.state}"/>
+    </div>
+    <ol class:hidden={!expanded} class="lg:flex">
+        <li class:border-b-4="{$activeRoute?currRoute=='/':false}" class="border-red-400"><a href="/" use:link>Home</a></li>
+        <li class:border-b-4="{$activeRoute?currRoute=='/about':false}" class="hidden border-orange-400"><a href="/about" use:link>About</a></li>
+        <li class:border-b-4="{$activeRoute?currRoute=='/education':false}" class="hidden border-yellow-400"><a href="/education" use:link>Education</a></li>
+        <li class:border-b-4="{$activeRoute?currRoute=='/work':false}" class="hidden border-green-400"><a href="/work" use:link>Work Experience</a></li>
+        <li class:border-b-4="{$activeRoute?currRoute=='/projects':false}" class="hidden border-teal-400"><a href="/projects" use:link>Projects</a></li>
+        <li class:border-b-4="{$activeRoute?currRoute=='/games':false}" class="hidden border-blue-400"><a href="/games" use:link>Games</a></li>
+        <li class:border-b-4="{$activeRoute?currRoute=='/blog':false}" class="hidden border-indigo-400"><a href="/blog" use:link>Blog</a></li>
+        <li class:border-b-4="{$activeRoute?currRoute=='/special':false}" class="border-purple-400"><a href="/special" use:link>Specials</a></li>
+    </ol>
+</div>
---
+++ src/pages/specials.svelte
@@ -0,0 +1,84 @@
+<script>
+import { onMount } from 'svelte';
+let view=false;
+onMount(async ()=>{
+    let pass=prompt("enter pass");
+    if(pass=="rose")
+        view=true;
+});
+</script>
+<style lang="text/postcss">
+.content {
+    @apply w-screen;
+    display: flex;
+}
+.content:nth-child(odd) {
+    min-height: 20%;
+}
+.content:nth-child(even) {
+    min-height: 40%;
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+#img1 {
+    background-image: url("https://www.w3schools.com/howto/img_parallax.jpg");
+    min-height: 100%;
+}
+#img3 {
+    background-image: url("https://www.w3schools.com/howto/img_parallax2.jpg");
+    min-height: 50%;
+}
+#img5 {
+    background-image: url("https://www.w3schools.com/howto/img_parallax3.jpg");
+    min-height: 50%;
+}
+#img7 {
+    background-image: url("https://www.w3schools.com/howto/img_parallax.jpg");
+    min-height: 100%;
+}
+.font-pacific {
+    font-family: 'Pacifico';
+}
+</style>
+{#if view}
+<div class="block font-pacific">
+    <div class="w-screen h-16">
+    </div>
+    <div class="content" id="img1">
+    <span class="m-auto">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi culpa iure tempore, reprehenderit, nesciunt ipsum expedita, perspiciatis similique perferendis consequatur distinctio possimus? Et magnam, dolores culpa aperiam odio dolorum eaque!
+    </span>
+    </div>
+    <div class="content bg-blue-400">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi culpa iure tempore, reprehenderit, nesciunt ipsum expedita, perspiciatis similique perferendis consequatur distinctio possimus? Et magnam, dolores culpa aperiam odio dolorum eaque!
+    </div>
+    <div class="content" id="img3">
+    <span>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi culpa iure tempore, reprehenderit, nesciunt ipsum expedita, perspiciatis similique perferendis consequatur distinctio possimus? Et magnam, dolores culpa aperiam odio dolorum eaque!
+    </span>
+    </div>
+    <div class="content bg-pink-400">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi culpa iure tempore, reprehenderit, nesciunt ipsum expedita, perspiciatis similique perferendis consequatur distinctio possimus? Et magnam, dolores culpa aperiam odio dolorum eaque!
+    </div>
+    <div class="content" id="img5">
+    <span>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi culpa iure tempore, reprehenderit, nesciunt ipsum expedita, perspiciatis similique perferendis consequatur distinctio possimus? Et magnam, dolores culpa aperiam odio dolorum eaque!
+    </span>
+    </div>
+    <div class="content bg-purple-400">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi culpa iure tempore, reprehenderit, nesciunt ipsum expedita, perspiciatis similique perferendis consequatur distinctio possimus? Et magnam, dolores culpa aperiam odio dolorum eaque!
+    </div>
+    <div class="content" id="img7">
+    <span class="m-auto">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi culpa iure tempore, reprehenderit, nesciunt ipsum expedita, perspiciatis similique perferendis consequatur distinctio possimus? Et magnam, dolores culpa aperiam odio dolorum eaque!
+    </span>
+    </div>
+</div>
+{:else}
+<div class="m-auto">
+    Nothing to see here
+</div>
+{/if}
