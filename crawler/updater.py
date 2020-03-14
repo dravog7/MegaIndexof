a9a3e810b177b629dfbce48778674b9961ba5b90
dravog7 <dravog77@gmail.com>
2020-03-15 00:08:22 +0530

    modify updater

--- crawler/updater.py
+++ crawler/updater.py
@@ -1,6 +1,6 @@
-import asyncio
 from threading import Thread
 from mains.models import videos
+import time
 class Updater(Thread):
     def __init__(self,out):
         Thread.__init__(self)
@@ -8,11 +8,19 @@ class Updater(Thread):
         self.arr=[]
     
     def run(self):
+        time1 = time.time() #update every 2 mins
         while True:
-            self.process(self.out.get())
-            if(len(self.arr)>10):
+            try:
+                val = self.out.get(timeout=10)
+                self.process(val)
+            except:
+                pass
+            time2 = time.time()
+            if((len(self.arr)>0)and(abs(time2-time1)>=60)):
+                print("bulk create")
                 videos.objects.bulk_create(self.arr,ignore_conflicts=True)
                 self.arr = []
+                time1 = time.time()
     
     def process(self,fields):
         a = videos(**fields)
